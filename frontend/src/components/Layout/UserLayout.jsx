d3c9870b167a1a0f7bbc4015b0514de2c17f3a29
Aakash <aakashpawar496@gmail.com>
2025-03-09 12:42:37 +0530

    Hero section added

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Footer from "../Common/Footer";
 import Header from "../Common/Header";
 
@@ -8,7 +9,7 @@ const UserLayout = () => {
       <Header />
       {/* Main Content */}
       <main>
-
+        <Outlet />
       </main>
       {/* Footer */}
       <Footer />
