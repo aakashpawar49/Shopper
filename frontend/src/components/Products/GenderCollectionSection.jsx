27f5d72163b3b94cacb56cebb9ba220a61ab9fad
Aakash <aakashpawar496@gmail.com>
2025-03-10 01:14:53 +0530

    Details about product

--- frontend/src/components/Products/GenderCollectionSection.jsx
+++ frontend/src/components/Products/GenderCollectionSection.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import mensCollectionImage from "../../assets/mens-collection.webp";
 import womensCollectionImage from "../../assets/womens-collection.webp";
 
-const GenderCollection = () => {
+const GenderCollectionSection = () => {
   return <section className="py-16 px-4 lg:px-0">
     <div className="container mx-auto flex flex-col md:flex-row gap-8">
         {/* Women's Collection */}
@@ -41,4 +41,4 @@ const GenderCollection = () => {
   </section>
 }
 
-export default GenderCollection
+export default GenderCollectionSection
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
-import Hero from "../components/Layout/Hero"
-import GenderCollection from "../components/Products/GenderCollection"
+import Hero from "../components/Layout/Hero";
+import GenderCollection from "../components/Products/GenderCollectionSection"
 import NewArrivals from "../components/Products/NewArrivals"
+import ProductDetails from "../components/Products/ProductDetails";
 
 const Home = () => {
   return (
@@ -8,8 +9,13 @@ const Home = () => {
         <Hero />
         <GenderCollection />
         <NewArrivals />
+
+        {/* Best Seller */}
+        <h2 className="text-3xl text-center font-bold mb-4">Best
+          Seller</h2>
+        <ProductDetails />
     </div>
   )
 }
 
-export default Home
+export default Home;
