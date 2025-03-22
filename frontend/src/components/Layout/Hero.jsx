1dbac865d417d3a1b78ed34de242cae268b43a98
Aakash <aakashpawar496@gmail.com>
2025-03-22 23:58:37 +0530

    Minor changes

--- frontend/src/components/Layout/Hero.jsx
+++ frontend/src/components/Layout/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className="relative">
       <img 
         src={heroImg}
-        alt="Rabbit" 
+        alt="Shopper" 
         className="w-full h-[400px] md:h-[600px] lg:h-[750px] object-cover"
       />
       <div className="absolute inset-0 bg-black bg-opacity-5 flex items-center justify-center">
@@ -17,7 +17,7 @@ const Hero = () => {
           <p className="text-sm tracking-tighter md:text-lg mb-6">
             Explore our vacation outfits with fast worldwide shipping.
           </p>
-          <Link to="/shop" className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg">
+          <Link to="#" className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg">
             Shop Now
           </Link>
         </div>
