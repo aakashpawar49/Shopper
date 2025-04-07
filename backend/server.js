073a80fc844b27821eb901522928f2d658275170
Aakash <aakashpawar496@gmail.com>
2025-04-07 12:34:07 +0530

    cart route added

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
 const checkoutRoutes = require("./routes/checkoutRoutes");
 
 const app = express();
