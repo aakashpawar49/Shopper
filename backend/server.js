60e60d97c13b7106cb4b22fc2d0652db463f6ffa
Aakash <aakashpawar496@gmail.com>
2025-04-13 13:55:56 +0530

    Admin routes added, Subscriber added and Checkout-finalized bug fixed

--- backend/server.js
+++ backend/server.js
@@ -7,20 +7,23 @@ const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const checkoutRoutes = require("./routes/checkoutRoutes");
 const orderRoutes = require("./routes/orderRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
+const subscriberRoutes = require("./routes/subscriberRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 connectDB();
 
-app.get("/", (req,res) => {
-    res.send("Welcome to Shopper API ")
+app.get("/", (req, res) => {
+    res.send("Welcome to Shopper API");
 });
 
 // API Routes
@@ -29,7 +32,12 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/checkout", checkoutRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api", subscriberRoutes);
 
-app.listen(PORT, ()=> {
+// Admin
+app.use("/api/admin/users", adminRoutes);
+
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
