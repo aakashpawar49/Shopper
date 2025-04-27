0ac1041ffc4562aba52dc190ca0a9757fc2dfe19
Aakash <aakashpawar496@gmail.com>
2025-04-27 11:21:33 +0530

    ruppee

--- frontend/src/components/Products/NewArrivals.jsx
+++ frontend/src/components/Products/NewArrivals.jsx
@@ -132,7 +132,7 @@ const NewArrivals = () => {
                         <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-4 rounded-b-lg">
                             <Link to={`/product/${product._id}`} className="block">
                                 <h4 className="font-medium">{product.name}</h4>
-                                <p className="mt-1">${product.price}</p>
+                                <p className="mt-1">₹{product.price}</p>
                             </Link>
                         </div>
                     </div>
